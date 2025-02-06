feae9968c5b91da4affb578d2c8b64ab34684b25
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-02-06 13:51:59 +0100

    feat: add Form component and import in Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Form from "./Form";
 
 const initialData = {
   id: 0,
@@ -142,88 +143,12 @@ export default function Main() {
         <div className="container">
           {/* FORM */}
 
-          <form onSubmit={handleSubmit} className="form__addPost">
-            <input
-              className="form__inputArea"
-              type="text"
-              name="title"
-              placeholder="Inserisci titolo nuovo articolo"
-              value={formData.title}
-              onChange={handleFormData}
-              required
-            />
-            <input
-              className="form__inputArea"
-              type="text"
-              name="author"
-              placeholder="Inserisci autore nuovo articolo"
-              value={formData.author}
-              onChange={handleFormData}
-              required
-            />
-            <input
-              className="form__inputArea"
-              type="url"
-              name="image"
-              placeholder="Inserisci URL immagine"
-              value={formData.image}
-              onChange={handleFormData}
-              required
-            />
-            <textarea
-              className="form__inputArea form__contentArea"
-              type="text"
-              name="content"
-              placeholder="Inserisci contenuto nuovo articolo"
-              value={formData.content}
-              onChange={handleFormData}
-              required
-            />
-            <input
-              className="form__inputArea"
-              type="text"
-              name="tags"
-              placeholder="Inserisci i tag - ricorda di separarli con la virgola"
-              value={formData.tags}
-              onChange={handleFormData}
-              required
-            />
-            <div className="form__select">
-              <label htmlFor="category">Scegli una categoria:</label>
-              <select
-                id="category"
-                name="category"
-                className="form__inputArea form__inputArea--selector"
-                value={formData.category}
-                onChange={handleFormData}
-                required
-              >
-                <option value="" disabled>
-                  ---
-                </option>
-                <option value="Dolce">Dolce</option>
-                <option value="Snack">Snack</option>
-                <option value="Pasta">Pasta</option>
-                <option value="Verdura">Verdura</option>
-                <option value="Pesce">Pesce</option>
-                <option value="Carne">Carne</option>
-              </select>
-            </div>
-            <div className="form__checkbox">
-              <label htmlFor="pubblicato">Pubblicato</label>
-              <input
-                id="pubblicato"
-                type="checkbox"
-                name="available"
-                className="form__checkbox--input"
-                checked={formData.available}
-                onChange={handleFormData}
-              />
-            </div>
-            <button className="form__submitBtn" type="submit">
-              Aggiungi articolo
-            </button>
-          </form>
+          <Form
+            onSubmit={handleSubmit}
+            className="form__addPost"
+            formData={formData}
+            handleFormData={handleFormData}
+          />
 
           {/* /FORM */}
         </div>
